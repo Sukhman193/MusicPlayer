546a07023d68b9a0d3586aedd553a311e6c8035a
Sukhmandeep <sukhmandeeps02@gmail.com>
2022-04-01 02:03:45 -0700

    Working on webViews

---
+++ MusicPlayer/Links.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer
+{
+    internal class Links
+    {
+        public string Title;
+        public string HtmlLink;
+
+        public Links(string _title, string _htmlLink)
+        {
+            this.Title = _title;
+            this.HtmlLink = _htmlLink;
+        }
+
+        public string GetTitle()
+        {
+            return this.Title;
+        }
+
+        public string GetHtmlLink()
+        {
+            return this.HtmlLink;
+        }
+    }
+}
--- MusicPlayer/MainPage.xaml.cs
+++ MusicPlayer/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +23,96 @@ namespace MusicPlayer
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        List<Links> webViewLinks;
+
         public MainPage()
         {
             this.InitializeComponent();
+            retrieveData();
+        }
+
+        private async void retrieveData()
+        {
+            StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            StorageFile myFile = await localFolder.CreateFileAsync("webViewLinks.txt", CreationCollisionOption.OpenIfExists);
+            IList<string> allLinkList = await FileIO.ReadLinesAsync(myFile);
+            webViewLinks = new List<Links>();  
+            foreach (string link in allLinkList)
+            {
+                string[] stringSplit = link.Split("/-}/");
+                Links currentLink = new Links(stringSplit[0], stringSplit[1]);
+                webViewLinks.Add(currentLink);
+            }
+            createButtons();
+        }
+
+
+        private void createButtons()
+        {
+            Button button = new Button();
+            button.Content = "<-";
+            button.Foreground = new SolidColorBrush(Windows.UI.Colors.Black);
+            button.Background = new SolidColorBrush(Windows.UI.Colors.Beige);
+            button.Name = "goBack";
+            button.FocusVisualPrimaryBrush = new SolidColorBrush(Windows.UI.Colors.White);
+            button.FocusVisualSecondaryBrush = new SolidColorBrush(Windows.UI.Colors.Blue);
+            
+            button.HorizontalAlignment = HorizontalAlignment.Center;
+            menuStackPanel.Children.Add(button);
+
+            Button button1 = new Button
+            {
+                Content = "Music",
+                Foreground = new SolidColorBrush(Windows.UI.Colors.Black),
+                Background = new SolidColorBrush(Windows.UI.Colors.Beige),
+                FocusVisualPrimaryBrush = new SolidColorBrush(Windows.UI.Colors.White),
+                Name = "Music",
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            menuStackPanel.Children.Add(button1);
+
+            foreach (Links link in webViewLinks)
+            {
+                Button btn = new Button
+                {
+                    Content = link.GetTitle(),
+                    Foreground = new SolidColorBrush(Windows.UI.Colors.Black),
+                    Background = new SolidColorBrush(Windows.UI.Colors.Beige),
+                    FocusVisualPrimaryBrush = new SolidColorBrush(Windows.UI.Colors.White)
+                };
+                btn.Click += WebViewButtonClick;
+                btn.HorizontalAlignment = HorizontalAlignment.Center;
+                menuStackPanel.Children.Add((btn));
+            }
+
+            Button addItem = new Button
+            {
+                Content = "+",
+                FontSize = 40,
+                Foreground = new SolidColorBrush(Windows.UI.Colors.White),
+                Background = new SolidColorBrush(Windows.UI.Colors.DarkKhaki),
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Bottom
+            };
+            menuStackPanel.Children.Add(addItem);
+            
+        }
+        
+
+        private void WebViewButtonClick(object sender, RoutedEventArgs e)
+        {
+
+            Button button = (Button)sender;
+            string title = (string)button.Content;
+            string htmlLink = "";
+
+            foreach(Links link in webViewLinks)
+            {
+                if (title == link.GetTitle())
+                    htmlLink = link.GetHtmlLink();
+            }
+
+            myWebView.Navigate(new Uri(htmlLink));
         }
     }
 }
