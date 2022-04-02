0a9c01d71b53db5c1f45d0ac99592b725a6477e1
Sukhmandeep <sukhmandeeps02@gmail.com>
2022-04-01 17:18:47 -0700

    created add new link page

--- MusicPlayer/MainPage.xaml.cs
+++ MusicPlayer/MainPage.xaml.cs
@@ -24,6 +24,7 @@ namespace MusicPlayer
     public sealed partial class MainPage : Page
     {
         List<Links> webViewLinks;
+        Button goBackButton;
 
         public MainPage()
         {
@@ -31,6 +32,7 @@ namespace MusicPlayer
             retrieveData();
         }
 
+        // This function will retrieve the data from the application at startup
         private async void retrieveData()
         {
             StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
@@ -45,31 +47,39 @@ namespace MusicPlayer
             }
             createButtons();
         }
-
-
+        // The following function will create the necessary buttons from the data retrieved
         private void createButtons()
         {
-            Button button = new Button();
-            button.Content = "<-";
-            button.Foreground = new SolidColorBrush(Windows.UI.Colors.Black);
-            button.Background = new SolidColorBrush(Windows.UI.Colors.Beige);
-            button.Name = "goBack";
-            button.FocusVisualPrimaryBrush = new SolidColorBrush(Windows.UI.Colors.White);
-            button.FocusVisualSecondaryBrush = new SolidColorBrush(Windows.UI.Colors.Blue);
-            
-            button.HorizontalAlignment = HorizontalAlignment.Center;
-            menuStackPanel.Children.Add(button);
+            goBackButton = new Button
+            {
+                Content = "<-",
+                Foreground = new SolidColorBrush(Windows.UI.Colors.Black),
+                Background = new SolidColorBrush(Windows.UI.Colors.Beige),
+                Name = "goBack",
+                Margin = new Thickness { Left = 0, Top = 5, Right = 0, Bottom = 5 },
+                Padding = new Thickness { Left = 10, Right = 10, Bottom = 10, Top = 10 },
+                FocusVisualPrimaryBrush = new SolidColorBrush(Windows.UI.Colors.White),
+                FocusVisualSecondaryBrush = new SolidColorBrush(Windows.UI.Colors.Blue),
+                HorizontalAlignment = HorizontalAlignment.Center,
+                IsEnabled = false
+            };
+            goBackButton.Click += GoBackButtonClick;
+            menuStackPanel.Children.Add(goBackButton);
+
 
             Button button1 = new Button
             {
                 Content = "Music",
                 Foreground = new SolidColorBrush(Windows.UI.Colors.Black),
                 Background = new SolidColorBrush(Windows.UI.Colors.Beige),
+                Margin = new Thickness { Left = 0, Top = 5, Right = 0, Bottom= 5 },
+                Padding = new Thickness { Left = 10, Right = 10, Bottom = 10, Top = 10 },
                 FocusVisualPrimaryBrush = new SolidColorBrush(Windows.UI.Colors.White),
                 Name = "Music",
                 HorizontalAlignment = HorizontalAlignment.Center
             };
             menuStackPanel.Children.Add(button1);
+
 
             foreach (Links link in webViewLinks)
             {
@@ -77,6 +87,8 @@ namespace MusicPlayer
                 {
                     Content = link.GetTitle(),
                     Foreground = new SolidColorBrush(Windows.UI.Colors.Black),
+                    Margin = new Thickness { Left = 0, Top = 5, Right = 0, Bottom = 5 },
+                    Padding = new Thickness { Left = 10, Right = 10, Bottom = 10, Top = 10},
                     Background = new SolidColorBrush(Windows.UI.Colors.Beige),
                     FocusVisualPrimaryBrush = new SolidColorBrush(Windows.UI.Colors.White)
                 };
@@ -88,17 +100,30 @@ namespace MusicPlayer
             Button addItem = new Button
             {
                 Content = "+",
-                FontSize = 40,
+                FontSize = 30,
                 Foreground = new SolidColorBrush(Windows.UI.Colors.White),
                 Background = new SolidColorBrush(Windows.UI.Colors.DarkKhaki),
+                Margin = new Thickness { Bottom = 5 },
                 HorizontalAlignment = HorizontalAlignment.Center,
                 VerticalAlignment = VerticalAlignment.Bottom
             };
-            menuStackPanel.Children.Add(addItem);
+            addItem.Click += AddItem_Click;
+            menuGrid.Children.Add(addItem);
             
         }
-        
 
+
+        // This function will trigger when the user clicks on the go back button
+        private void GoBackButtonClick(object sender, RoutedEventArgs e)
+        {
+            myWebView.GoBack();
+            if (!myWebView.CanGoBack) 
+            {
+                goBackButton.IsEnabled = false; 
+            }
+        }
+
+        // this function will trigger when the user selectrs a page to open
         private void WebViewButtonClick(object sender, RoutedEventArgs e)
         {
 
@@ -112,7 +137,89 @@ namespace MusicPlayer
                     htmlLink = link.GetHtmlLink();
             }
 
+            addItemPanel.Visibility = Visibility.Collapsed;
             myWebView.Navigate(new Uri(htmlLink));
+        }
+
+
+        // This function will trigger when a webView Page starts loading
+        private void myWebView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            progressRingWebView.Visibility = Visibility.Visible;
+            myWebView.Visibility = Visibility.Collapsed;
+            progressRingWebView.IsActive = true;
+        }
+
+        // This function will trigger when a webView Page ends loading
+        private void myWebView_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            progressRingWebView.Visibility = Visibility.Collapsed;
+            myWebView.Visibility = Visibility.Visible;
+            progressRingWebView.IsActive = false;
+
+            // Check if the webView can go back
+            if (myWebView.CanGoBack)
+            {
+                goBackButton.IsEnabled = true;
+            }
+        }
+
+
+        // This function will trigger when the user decides to create a new object
+        private void AddItem_Click(object sender, RoutedEventArgs e)
+        {
+            myWebView.Visibility = Visibility.Collapsed;
+            addItemPanel.Visibility = Visibility.Visible;
+        }
+        private async void saveData(string title, string htmlLink)
+        {
+            StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;      // Put this with the other function
+            string fileData = "\n" + title + "/-}/" + htmlLink;
+            StorageFile myFile = await localFolder.CreateFileAsync("webViewLinks.txt", CreationCollisionOption.OpenIfExists);
+            await FileIO.AppendTextAsync(myFile, fileData);
+        }
+
+        // This function will trigger when the user clicks on the Create new link button
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            string _title = titleInput.Text;
+            string _htmlLink = linkInput.Text;
+            List<string> _titleList = new List<string>();
+
+            foreach(Links links in webViewLinks)
+            {
+                _titleList.Add(links.GetTitle());
+            }
+
+
+            if(_title.Trim() == "" || _titleList.Contains(_title)){
+                // create dialog box to say that we cannot use this title
+                return;
+            }
+
+            if (!_htmlLink.StartsWith("http"))
+            {
+                // crate dialog box to say that we cannot use this link, should start with http
+                return;
+            }
+
+            saveData(_title, _htmlLink);
+            Links newLink = new Links(_title, _htmlLink);
+            webViewLinks.Add(newLink);
+            Button btn = new Button
+            {
+                Content = _title,
+                Foreground = new SolidColorBrush(Windows.UI.Colors.Black),
+                Margin = new Thickness { Left = 0, Top = 5, Right = 0, Bottom = 5 },
+                Padding = new Thickness { Left = 10, Right = 10, Bottom = 10, Top = 10 },
+                Background = new SolidColorBrush(Windows.UI.Colors.Beige),
+                FocusVisualPrimaryBrush = new SolidColorBrush(Windows.UI.Colors.White)
+            };
+            btn.Click += WebViewButtonClick;
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            menuStackPanel.Children.Add((btn));
+
+
         }
     }
 }
